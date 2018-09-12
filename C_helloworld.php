3bb060b2d14b4f6cf2afdc67e78f5cca9982ff22
nickostrife <40533459+nickostrife@users.noreply.github.com>
2018-09-12 16:45:59 +0700

    Update BAB 4
    menambahkan beberapa controller dan views

--- C_helloworld.php
+++ C_helloworld.php
@@ -1,9 +1,76 @@
 <?php
 class C_helloworld extends CI_Controller{
-    function index(){
-        echo "Ini adalah controller Index!";
+    function index() {
+        ?>
+        <html>
+        <head>
+            <title>Hello World Berbagai Bahasa!</title>
+        </head>
+        <body>
+            <?php echo "Hello World English";?>
+        </body>
+        </html>
+        <?php
     }
 
+    function daftar() {
+        $this->load->model ("m_hello");
+        $data=array();
+        $judulapp="Daftar Bahasa dan Kosa Kalimat";
+        $data["judulapp"]=$judulapp;
+        $data["abahasa"]=$this->m_hello->abahasa;
+        $data["akalimat"]=$this->m_hello->akalimat;
+        $this->load->view("v_c_v_hellodaftar", $data);
+    }
+
+    function kamus() {
+        $this->load->model("m_hello");
+        $data=array();
+        $data["abahasa"]=$this->m_hello->abahasa;
+        $data["akalimat"]=$this->m_hello->akalimat;
+        $this->load->view("v_c_v_hellokamus", $data);
+    }
+
+    function talkmix($langid="EN") {
+        $this->load->model("m_hello");
+        $data=array();
+        $data["halo"]=$this->m_hello->katakan($langid);
+        $this->load->view("v_c_v_hellovar", $data);
+        }
+
+    function talka($langid) {
+        $this->load->model("m_hello");
+        $data=array();
+        $data["halo"]=$this->m_hello->abahasa[$langid];
+        $data["halo"]=$data["halo"].">>".$this->m_hello->akalimat[$langid];
+        $this->load->view("v_c_v_hellovar", $data);
+    }
+
+    function talk($langid="EN") {
+        $this->load->model ("m_hello");
+        $data["halo"]=$this->m_hello->bilang($langid);
+        $this->load->view("v_c_v_hellovar", $data);
+    }
+
+    function says($langid="EN") {
+        $this->load->model ("m_hello");
+        $data=array();
+        $langname="English ";
+        $data["halo"]=$langname.">>".$this->m_hello->halo;
+        switch ($langid) {
+            case "ID":
+                $langname="Indonesia ";
+                $data["halo"]=$langname.">>"."Halo dunia!";
+                break;
+            case "AR":
+                $langname="Arabian ";
+                $data["halo"]=$langname.">>"."Ahlan dunia!";
+                break;
+        }
+        $this->load->view("v_c_v_hellovar", $data);
+    }
+        
+    
     function viewmurni() {
         //pengganti fungsi c_v_hello
         $this->load->view ("v_c_v_hello");
--- M_hello.php
+++ M_hello.php
@@ -1,8 +1,44 @@
 <?php
 class M_hello extends CI_Model{
-    var $halo="Hello World! Model!";
+    var $halo="Hello World!";
+
+    var $abahasa=array(
+        "EN"=>"Hello World!",
+        "ID"=>"Halo Dunia!",
+        "AR"=>"Ahlan Dunia!"
+    );
+
+    function katakan($langid) {
+        $alangid=array_keys($this->abahasa);
+        $langid=in_array($langid, $alangid)?$langid:"EN";
+        $kalimat=$this->abahasa[$langid].">>".$this->akalimat[$langid];
+        return $kalimat;
+    }
+
+    var $akalimat=array(
+        "EN"=>"Inggris",
+        "ID"=>"Indonesia",
+        "AR"=>"Arab"
+    );
+
     function katakata() {
         $kalimat=$this->halo." I'm from model!";
+        return $kalimat;
+    }
+
+    function bilang($langid) {
+        $langname="English ";
+        $kalimat=$langname.">>".$this->m_hello->halo;
+        switch ($langid) {
+            case "ID":
+                $langname="Indonesia ";
+                $kalimat=$langname.">>"."Halo dunia!";
+                break;
+            case "AR":
+                $langname="Arabian "    ;
+                $kalimat=$langname.">>"."Ahlan dunia!";
+                break;
+        }
         return $kalimat;
     }
 }
